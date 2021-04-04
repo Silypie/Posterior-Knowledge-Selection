10f07ffd1f3bfaec06cb27325eb3d69faa63817b
chaobin <1822158323@qq.com>
2021-04-04 22:57:23 +0800

    update

--- tmp.py
+++ tmp.py
@@ -4,7 +4,7 @@ import json
 import os
 import params
 from threading import main_thread
-from utils import get_data_loader
+from utils import WizardDataset, build_vocab, get_data_loader
 from tqdm import tqdm
 from nltk import word_tokenize
 
@@ -38,13 +38,28 @@ def cal_length():
         print('y的最大长度,',y_len)
         print('每轮对话的最大知识数量,',num)
 
+def origin(tgt_y, vocab):
+    tgts = tgt_y # [max_len]
+    target = ''
+    for tgt in tgts:
+        if tgt.item() == params.EOS:
+            break
+        target += vocab.itos[tgt.item()] + " "
+    target = target[:-1]
+    return target
+
+def origin_sentence():
+    dataset = WizardDataset(params.test_samples_path)
+    vocab = build_vocab(params.train_path, params.n_vocab)
+    for i in range(3):
+        src_X, src_y, src_K, tgt_y = dataset[i]
+        print('X: ',origin(src_X, vocab))
+        print('y: ',origin(src_y, vocab))
+        print('K: ',origin(src_K[0], vocab))
+        print('tgt_y: ',origin(tgt_y, vocab))
 
 if __name__ == '__main__':
     # cal_length()
     # datas = json.load('./data/')
-    num = 0
-    samples_dirs = os.listdir(params.train_samples_path)
-    for dir in samples_dirs:
-        num = num + len(os.listdir(params.train_samples_path + dir))
-    print(num)
+    origin_sentence()
     pass
